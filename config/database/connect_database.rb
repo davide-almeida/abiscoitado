b85d370dda2e7871017d8bfc6c0a8fbc434a084e
Davide Almeida <davide.jhonatan@gmail.com>
2024-02-27 21:24:07 -0300

    Ah mas essa é muito fácil, faça outra mais difícil!

--- config/database/connect_database.rb
+++ config/database/connect_database.rb
@@ -1,12 +1,18 @@
 require 'pg'
+require_relative '../../lib/dotenv_reader'
 
+# .env
+DotenvReader.load
+
+#db
 class ConnectDatabase
-  def initialize
-    @connection = PG.connect(
+  def self.connection
+    @connection ||= PG.connect(
       dbname: ENV['POSTGRES_DB'],
       user: ENV['POSTGRES_USER'],
       password: ENV['POSTGRES_PASSWORD'],
-      host: ENV['POSTGRES_HOST']
+      host: ENV['POSTGRES_HOST'],
+      port: ENV['POSTGRES_PORT']
     )
   end
 end
--- config/database/init.sql
+++ config/database/init.sql
@@ -3,21 +3,23 @@ CREATE TABLE clients (
   name VARCHAR(255) NOT NULL,
   address VARCHAR(255),
   phone_number VARCHAR(255),
-  email VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT current_timestamp,
+  updated_at TIMESTAMP DEFAULT current_timestamp
 );
 
 DO $$
 BEGIN
-  INSERT INTO clients (name, address, phone_number, email)
+  INSERT INTO clients (name, address, phone_number, email, created_at, updated_at)
   VALUES
-    ('Davide', 'Rua dos Testes, 1', '(11)111111', 'davidedostestes@abiscoitado.com' ),
-    ('Luiza', 'Rua dos Testes, 2', '', 'luiza@abiscoitado.com'),
-    ('Caê', 'Rua dos Testes, 3', '', 'cae@abiscoitado.com'),
-    ('Pinato', 'Rua dos Testes, 4', '', 'pinato@abiscoitado.com'),
-    ('Hika', '', '(55)555555', 'hikari@abiscoitado.com'),
-    ('Icaro', '', '(66)666666', 'icaro@abiscoitado.com'),
-    ('Yan', 'Rua dos Testes, 7', '(77)777777', 'yan@abiscoitado.com'),
-    ('Lucas', 'Rua dos Testes, 8', '(88)888888', 'lucas@abiscoitado.com'),
-    ('Edi', 'Rua dos Testes, 9', '(99)999999', 'edi@abiscoitado.com'),
-    ('Leandro', 'Rua dos Testes, 10', '(10)101010', 'leandro@abiscoitado.com');
+    ('Davide', 'Rua dos Testes, 1', '(11)111111', 'davidedostestes@abiscoitado.com', current_timestamp, current_timestamp),
+    ('Luiza', 'Rua dos Testes, 2', '', 'luiza@abiscoitado.com', current_timestamp, current_timestamp),
+    ('Caê', 'Rua dos Testes, 3', '', 'cae@abiscoitado.com', current_timestamp, current_timestamp),
+    ('Pinato', 'Rua dos Testes, 4', '', 'pinato@abiscoitado.com', current_timestamp, current_timestamp),
+    ('Hika', '', '(55)555555', 'hikari@abiscoitado.com', current_timestamp, current_timestamp),
+    ('Icaro', '', '(66)666666', 'icaro@abiscoitado.com', current_timestamp, current_timestamp),
+    ('Yan', 'Rua dos Testes, 7', '(77)777777', 'yan@abiscoitado.com', current_timestamp, current_timestamp),
+    ('Lucas', 'Rua dos Testes, 8', '(88)888888', 'lucas@abiscoitado.com', current_timestamp, current_timestamp),
+    ('Edi', 'Rua dos Testes, 9', '(99)999999', 'edi@abiscoitado.com', current_timestamp, current_timestamp),
+    ('Leandro', 'Rua dos Testes, 10', '(10)101010', 'leandro@abiscoitado.com', current_timestamp, current_timestamp);
 END; $$
