b708f83267332d602eb42b5cc99525edb578ccb9
Davide Almeida <davide.jhonatan@gmail.com>
2024-02-27 21:21:48 -0300

    Sinatra config

--- server.rb
+++ server.rb
@@ -1,17 +1,25 @@
+# gems and libs
 require 'sinatra'
-require_relative 'app/controllers/hello_controller'
 require_relative 'config/database/connect_database'
 require_relative 'lib/dotenv_reader'
+# controllers
+require_relative 'app/controllers/hello_controller'
+require_relative 'app/controllers/clients_controller'
 
 # .env
 DotenvReader.load
 
 # Sinatrinha
 class Server < Sinatra::Base
+  configure :production, :development do
+    enable :logging
+  end
+
   set :bind, '0.0.0.0'
   set :port, ENV['SERVER_PORT'] || 3000
 
   use HelloController
+  use ClientsController
 
   run! if app_file == $0
 end
